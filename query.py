3030a163fcf53220177f7369e984b343cac4eab8
K S Ramalakshmi <rlsri2305@gmail.com>
2022-11-26 19:14:25 +0530

    nearing completion

--- query.py
+++ query.py
@@ -1,13 +1,20 @@
 import streamlit as st
 import pandas as pd
+from functions import view_all_events
 
 def query_page(conn, cursor):
     query = st.text_input('Input your query here:') 
 
     if st.button("Execute"):
         cursor.execute(query)
+        conn.commit()
+
         data = cursor.fetchall()
-        df = pd.DataFrame(data)
+
+        if query.lower() == 'select * from events;':
+            df = view_all_events(cursor)
+        else:
+            df = pd.DataFrame(data)
+
         st.dataframe(df)
         st.success("Query execution successful!")
-        conn.commit()
